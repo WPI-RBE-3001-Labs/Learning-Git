b262b3e12dec00c57c03d3ecf386d8be8c5e06d7
Gunnar <Gunnar@DESKTOP-B43G20R>
2017-01-30 14:51:53 -0500

    Wrote inefficient fibonacci calculator

--- Top Kek/src/main/main.java
+++ Top Kek/src/main/main.java
@@ -1,9 +1,22 @@
 package main;
 
+/*
+ * I don't know why anyone would read this.  If you do...
+ * kudos, I guess?  At any rate:  here's an inefficient
+ * fibonacci calculator.
+ */
 public class main {
 
 	public static void main(String[] args) {
-		System.out.println("Sup, boi?");
+		int i = 0;
+		while(true) {
+			System.out.println("The " + i + "th fibonacci number is: " + main.fib(i++));
+		}
 	}
 	
+	private static int fib(int n) {
+		if(n == 0) { return 0; } // base case
+		if(n == 1) { return 1; } // baser case
+		return fib(n - 1) + fib(n - 2);
+	}
 }
